996ff8d409b763eeebb253088efaec287616fb93
Mashood Rafi <mashood@turnlunchon.com>
2021-06-15 16:45:03 +0500

    Remove unnecessary packages.

--- src/server/expressApp.js
+++ src/server/expressApp.js
@@ -1,26 +1,14 @@
-import config from '../config/config';
 import Connection from './connection';
 
 const express = require('express');
-const session = require('express-session');
 const bodyParser = require('body-parser');
-const multer = require('multer');
 const cors = require('cors');
-const shortId = require('shortid');
 
 const app = express();
 app.use(cors());
 app.use(express.static('public'));
 app.set('view engine', 'pug');
 app.set('trust proxy', 1); // trust first proxy
-app.use(
-    session({
-        secret: 'keyboard cat',
-        resave: false,
-        saveUninitialized: true,
-        cookie: {},
-    }),
-);
 
 Connection.buildConnections();
 app.use(bodyParser.json());
@@ -29,15 +17,5 @@ app.use(
         extended: true,
     }),
 );
-
-// multipart data handling
-const storage = multer.diskStorage({
-    destination: config.filePath,
-    filename: (req, file, cb) => {
-        cb(null, shortId.generate());
-    },
-});
-
-app.use(multer({ storage }).any());
 
 module.exports = app;
