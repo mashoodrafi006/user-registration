f103e5c443f6d0b27bd3c96823ed37d17781eeba
Mashood Rafi <mashood@turnlunchon.com>
2021-07-05 11:18:21 +0500

    Add body parse.

--- src/server/expressApp.js
+++ src/server/expressApp.js
@@ -2,6 +2,7 @@ import Connection from './connection';
 
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 app.use(cors());
@@ -10,5 +11,11 @@ app.set('view engine', 'pug');
 app.set('trust proxy', 1); // trust first proxy
 
 Connection.buildConnections();
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    }),
+);
 
 module.exports = app;
