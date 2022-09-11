b65f7a8873ec5e5a6f82c5699f86745f2fc2c223
Mashood Rafi <mashood.rafi@careem.com>
2022-09-11 23:04:42 +0500

    Code cleanup.

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -3,7 +3,7 @@ import winston from 'winston';
 const logger = winston.createLogger({
     level: 'error',
     format: winston.format.json(),
-    defaultMeta: { service: 'home-like' },
+    defaultMeta: { service: 'rapid-api' },
     transports: [new winston.transports.File({ filename: 'error.log', level: 'error' }), new winston.transports.File({ filename: 'combined.log' })],
 });
 
